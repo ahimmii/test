8167e8479538348ec6f48f6c3e465ec0840bd96e
ahimmi <43048724+ahimmii@users.noreply.github.com>
2022-10-15 12:38:10 +0100

    Add files via upload

---
+++ tst.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd Desktop
+cd L-pasSage-main
+npm run dev
---
+++ tst2.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd Desktop
+cd Lpassage-Dashboard-master
+npm run start
